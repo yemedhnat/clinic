4ee7c0c6749b3939f25c6d68d26007b97cf167c8
hailemedhn <hailemedhntegegne@gamil.com>
2024-02-19 08:09:12 +0100

    Deploy commit

---
+++ index.js
@@ -0,0 +1,31 @@
+// Initialize and add the map
+let map;
+let symptoms;
+const apiKey = 'YOUR_API_KEY';
+
+
+
+async function initMap() {
+  // The location of Uluru
+  const position = { lat: 8.97, lng: 38.78 };
+  // Request needed libraries.
+  //@ts-ignore
+  const { Map } = await google.maps.importLibrary("maps");
+  const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+
+  // The map, centered at Uluru
+  map = new Map(document.getElementById("map"), {
+    zoom: 15,
+    center: position,
+    mapId: "DEMO_MAP_ID",
+  });
+
+  // The marker, positioned at Uluru
+  const marker = new AdvancedMarkerElement({
+    map: map,
+    position: position,
+    title: "Uluru",
+  });
+}
+
+initMap();
