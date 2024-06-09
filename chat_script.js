745574efcccd83180ba44d74b327b712b899f851
Hailemedhn Tegegne <HailemedhnTegegne@gmail.com>
2024-06-09 19:36:36 +0200

    fixed user page

--- chat_script.js
+++ chat_script.js
@@ -1,6 +1,6 @@
 let container_div = document.getElementById("container_div");
 const bottom_spacer = document.getElementsByClassName("spacer_bottom")[0];
-const message_delay = 1000
+const message_delay = 0
 
 function place_m_message(message){
     let div = document.createElement("div");
@@ -36,6 +36,39 @@ function place_choices(choice,event_flag){
 
 let current_diagnosis = []
 
+function treat_diagnosis(diagnosis){
+    let diagnosis_obj = []
+    for(let i = 0; i <diagnosis.length; i++){
+        diagnosis_obj.push({
+            disease: diagnosis[i],
+            count: 0
+        })
+    }
+    for(let i = 0; i < diagnosis_obj.length; i++){
+        for(let j = i + 1; j < diagnosis_obj.length; j++){
+            if(diagnosis_obj[i].disease == diagnosis_obj[j].disease){
+                diagnosis_obj[i].count++;
+                console.log("added")
+                diagnosis_obj[j].count = -1;
+            }
+        }
+    }
+    diagnosis_obj.sort((a,b)=>{return b - a});
+    let k = 0;
+    
+    while(k < diagnosis_obj.length){)
+        if(diagnosis_obj[k].count == -1){
+            diagnosis_obj.splice(k,1);
+        }
+        k++;
+    }
+
+    diagnosis = [];
+    for(let i = 0; i < diagnosis_obj.length; i++){
+        diagnosis.push(diagnosis_obj[i].disease)
+    }
+    return diagnosis
+}
 function show_pains(e){
     for(let i = 0; i < container_div.children.length; i++){
         if(container_div.children[i].className == "choices"){
@@ -44,12 +77,14 @@ function show_pains(e){
         }
     }
     place_user_message(e.target.innerHTML)
-    choice = e.target.innerHTML
+    choice = e.target.innerHTML;
     index = body_parts.indexOf(choice) > -1 ? body_parts.indexOf(choice) : -1;
-    flag = body_parts.indexOf(choice) > -1 ? "body" : "symptom"
+    index = body_parts.indexOf("No other symptoms") > -1 ? --index : index;
+    flag =  body_parts.indexOf(choice) > -1 ? "body" : "symptom"
     if(flag == "body"){
         if(choice == "No other symptoms"){
             place_m_message("This is my diagnosis." );
+            current_diagnosis = treat_diagnosis(current_diagnosis)
             setTimeout(()=>{
                 place_choices(current_diagnosis,false);
             let div = document.createElement("div");
@@ -80,7 +115,7 @@ function show_pains(e){
         for(let i = 0; i < all_symptoms.length; i++){
             for(let j = 0; j < all_symptoms[i].length; j++){
                 if(all_symptoms[i][j] == choice){
-                    index = i;
+                    let index_internal = i;
                     current_diagnosis = current_diagnosis.concat(all_diagnosis[i])
                     break label1;
                 }
@@ -94,7 +129,9 @@ function show_pains(e){
             place_m_message ("Do you have any of the any other symptoms?")
         },message_delay*3);
         setTimeout(()=>{
-            body_parts.unshift("No other symptoms");
+            if(body_parts.indexOf("No other symptoms") < 0){
+                body_parts.unshift("No other symptoms");
+            }
             place_choices(body_parts,true);
         },message_delay*5);
     }
